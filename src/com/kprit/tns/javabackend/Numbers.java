7aa63ccc59d83f48744ffc7e348a6b6d9b8a69a5
yataj <yataj@Jay>
2025-09-18 15:49:36 +0530

    "Numbers"

---
+++ src/com/kprit/tns/javabackend/Numbers.java
@@ -0,0 +1,16 @@
+package com.kprit.tns.javabackend;
+
+public class Numbers {
+	
+	// MEthod for Numbers
+	
+	void numbers() {
+	
+	for(int i=1; i<=10;i++) {
+		
+		System.out.println(i);
+		
+	 }
+	}
+
+}
---
+++ src/com/kprit/tns/javabackend/NumbersMain.java
@@ -0,0 +1,13 @@
+package com.kprit.tns.javabackend;
+
+public class NumbersMain {
+
+	public static void main(String[] args) {
+		// Object for Numbers Class
+		
+   Numbers Obj = new Numbers();
+     Obj.numbers();
+
+	}
+
+}
