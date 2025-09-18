b35b55af8cf04e973dff92917e88084c2147e4b5
yataj <yataj@Jay>
2025-09-18 15:42:05 +0530

    "Initial commit"

---
+++ src/com/kprit/tns/javabackend/FirstProgram.java
@@ -0,0 +1,12 @@
+package com.kprit.tns.javabackend;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		
+		// My first Program
+		System.out.println("Welcome to TNS Backend Training ..!");
+
+	}
+
+}
